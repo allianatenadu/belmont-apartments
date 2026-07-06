ffbbe82968b81ad0d1b986f559a2a8319a4b9a49
Little_pixy. <168483368+allianatenadu@users.noreply.github.com>
2026-07-06 10:05:36 +0000

    Configure Next.js deployment for Cloudflare Workers

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"RESEND_API_KEY":"re_CMLMAna1_8ZDxN7LC5obCqdFP8VuKUKjf","ADMIN_EMAIL":"belmontoapartments@gmail.com","FROM_EMAIL":"onboarding@resend.dev"};
+export const development = {"RESEND_API_KEY":"re_CMLMAna1_8ZDxN7LC5obCqdFP8VuKUKjf","ADMIN_EMAIL":"belmontoapartments@gmail.com","FROM_EMAIL":"onboarding@resend.dev"};
+export const test = {};
